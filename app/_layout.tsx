8ef7bb9c706a58c20b6b1cd2b63bad9b9598944a
peyman norozy <peyman.norozy@gmail.com>
2024-10-20 20:36:53 +0330

    delete unusable file and code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,79 +1,16 @@
 import * as SplashScreen from "expo-splash-screen";
-import { useState } from "react";
-import { Button, StyleSheet, View, FlatList } from "react-native";
+import {StyleSheet, View, Text} from "react-native";
 import "react-native-reanimated";
-import { StatusBar } from "expo-status-bar";
-import GoalItem from "@/components/GoalItem";
-import GoalInput from "@/components/GoalInput";
 
-interface COURSEGOALS {
-  text: string;
-  id: string;
-}
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [modalIsVisible, setModalIsVisible] = useState(false);
-  const [courseGoals, setCourseGoals] = useState<COURSEGOALS[]>([]);
-
-  function startAddGoalHandler() {
-    setModalIsVisible(true);
-  }
-
-  function endAddGoalHandler() {
-    setModalIsVisible(false);
-  }
-
-  function addGoalHandler(enteredGoalText: string) {
-    setCourseGoals((currentCourseGoals) => [
-      ...currentCourseGoals,
-      { text: enteredGoalText, id: Math.random().toString() },
-    ]);
-    endAddGoalHandler();
-  }
-
-  function deleteGoalHandler(id: string) {
-    setCourseGoals((currentCourseGoals) => {
-      return currentCourseGoals.filter((goal) => goal.id !== id);
-    });
-  }
-
   return (
-    <>
-      <StatusBar style={"light"} />
-      <View style={styles.appConstainer}>
-        <Button
-          title={"Add new Goal"}
-          color={"#a065ec"}
-          onPress={startAddGoalHandler}
-        />
-        <GoalInput
-          visible={modalIsVisible}
-          onAddGoal={addGoalHandler}
-          onCancel={endAddGoalHandler}
-        />
-        <View style={styles.goalsContainer}>
-          <FlatList
-            data={courseGoals}
-            renderItem={(itemData) => {
-              return (
-                <GoalItem
-                  text={itemData.item.text}
-                  id={itemData.item.id}
-                  onDeleteItem={deleteGoalHandler}
-                />
-              );
-            }}
-            keyExtractor={(item, index) => {
-              return item.id;
-            }}
-            alwaysBounceVertical={false}
-          />
-        </View>
+      <View>
+        <Text>sdfs</Text>
       </View>
-    </>
   );
 }
 
