0310d63bcf3c9180a35feb3c42e6a0deae27826b
peyman norozy <peyman.norozy@gmail.com>
2024-10-23 00:09:15 +0330

    use linear gradient and background image

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,32 @@
 import * as SplashScreen from "expo-splash-screen";
-import { StyleSheet, View } from "react-native";
+import { ImageBackground, StyleSheet, View } from "react-native";
 import "react-native-reanimated";
 import StartGameScreen from "@/screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   return (
-    <View style={styles.rootScreen}>
-      <StartGameScreen />
-    </View>
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
+      <ImageBackground
+        source={require("../assets/images/background.png")}
+        resizeMode={"cover"}
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-    backgroundColor: "#ddb52f",
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
